6c8d11a3bb550f22908f3781b937d512b636473c
Tomas Cambala <tcambala@gratex.com>
2018-07-19 09:50:56 +0200

    initial basic  Jest test

---
+++ TEST/_uri.test.js
@@ -0,0 +1,14 @@
+const uri = require("../src/_uri.js");
+
+
+
+test("match string and Regex", function(){
+    expect(uri.decomposeComponents)
+});
+
+
+test("recompose ", function(){
+    expect(uri.recomposeAuthorityComponents)
+});
+
+
