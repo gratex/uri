926f65081c7a353746095a11cd29b9ddd01aa840
Ďurčík Vladimír <vdurcik@gratex.com>
2018-07-30 11:02:31 +0200

    Removed unneccesary uri input data, fixed format for input test data

--- test/uri.test.js
+++ test/uri.test.js
@@ -1,55 +1,14 @@
 const uri = require('../src/uri');
 const equalsTestData = [
-    [
-        'foo://username:password@my.example.com:8042/over/there/index.x.dtb?type=animal&name=narwhal#nose',
-        'foo://username:password@my.example.com:8042/over/there/index.x.dtb?type=animal&name=narwhal#nose',
-        true
-    ],
-    [
-        'type=animal&name=narwhal',
-        'name=narwhal&type=animal',
-        true
-    ],
-    [
-        'eq(type,animal)&lt(count,3)',
-        'lt(count,3)&eq(type,animal)',
-        true
-    ],
-    [
-        'a=1&b=2&a=3',
-        'b=2&a=1&a=3',
-        true
-    ],
-    [
-        '',
-        'b=2&a=1&a=3',
-        false
-    ],
-    [
-        'b=3',
-        '',
-        false
-    ],
-    [
-        'name=foo&name=bar', // order should not be considered
-        'name=bar&name=foo',
-        true
-    ],
-    [
-        'a=5&a=5',
-        'a=5&a=5',
-        true
-    ],
-    [
-        'w=42&t=3',
-        'w=42&t=4',
-        false
-    ],
-    [
-        'type=animal&name=narwhal',
-        'name=narwhal&type=animal&typeij=y',
-        false
-    ]
+    [ 'type=animal&name=narwhal', 'name=narwhal&type=animal', true ],
+    [ 'eq(type,animal)&lt(count,3)', 'lt(count,3)&eq(type,animal)', true ],
+    [ 'a=1&b=2&a=3', 'b=2&a=1&a=3', true ],
+    [ '', 'b=2&a=1&a=3', false ],
+    [ 'b=3', '', false ],
+    [ 'name=foo&name=bar', 'name=bar&name=foo', true ], // order should not be considered
+    [ 'a=5&a=5', 'a=5&a=5', true ],
+    [ 'w=42&t=3', 'w=42&t=4', false ],
+    [ 'type=animal&name=narwhal', 'name=narwhal&type=animal&typeij=y', false ]
 ];
 
 test('_equalsQueryStr test', (() => {
