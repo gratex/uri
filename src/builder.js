27ad8a13bb1abf0c8f6fac6f2a351e79e5e9e112
Adrian Rakovsky <adrian.rakovsky@gratex.com>
2018-07-30 10:00:05 +0200

    [TASK] builder::raw implemented

--- src/builder.js
+++ src/builder.js
@@ -1,24 +1,37 @@
 const uri = require('../src/_uri');
+
 const PLACEHOLDER = '$$$'; // should be safe string, not occuring in url templates, and not breaking decomposition
-const REPLACE = new RegExp(PLACEHOLDER, 'g');
+const REPLACE = /\$\$\$/g;
+
+class EncodedString {
+    // represents String with encoded flag,
+    // new Class to be able to make instanceOf could be also new String() + ._enc, but this seems cleaner
+    // needed by encode function to know if encode or not
+
+    constructor(value) {
+        this.string = `${value}`; // template to get string from value
+    }
+}
+
+function encode(value, encoder) {
+    return value instanceof EncodedString ? value.string : encoder(`${value}`); // template to get string from value
+}
 
 function uriBuilder(strings, ...values) {
-    // uriBuilder`/a/${x}/b/?u=${y}p=10#/a/b/${m}`
-
     const uriTemplate = strings.reduce((last, actual) => `${last}${PLACEHOLDER}${actual}`);
-    // -> '/a/$$$/b/?u=$$$p=10#/a/b/$$$'
 
     // eslint-disable-next-line prefer-const
     let { path, query, fragment, ...otherComponents } = uri.decomposeComponents(uriTemplate);
 
-    path = path && path.replace(REPLACE, () => uri.encodeSegment(values.shift()));
-    query = query && query.replace(REPLACE, () => uri.encodeQuery(values.shift()));
-    fragment = fragment && fragment.replace(REPLACE, () => uri.encodeFragment(values.shift()));
+    path = path && path.replace(REPLACE, () => encode(values.shift(), uri.encodeSegment));
+    query = query && query.replace(REPLACE, () => encode(values.shift(), uri.encodeQuery));
+    fragment = fragment && fragment.replace(REPLACE, () => encode(values.shift(), uri.encodeFragment));
 
     return uri.recomposeComponents({ ...otherComponents, path, query, fragment });
 }
 
-function raw() {
+function raw(string) {
+    return new EncodedString(string);
 }
 
 module.exports = {
