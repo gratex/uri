026618ccf68a210750b15d6a685eb2241ee50ae6
Michal Zajic <mzajic@gratex.com>
2018-08-07 09:32:39 +0200

    fixed small uri d.ts file

--- typings/_uri.d.ts
+++ typings/_uri.d.ts
@@ -5,93 +5,47 @@
 // decomposeComponents.!ret
 
 
-export declare var splitUriRegex : RegExp;
+export declare const splitUriRegex : RegExp;
+export declare const RFC2396_DIGIT : string;
+export declare const RFC2396_LOWALPHA : string;
+export declare const RFC2396_UPALPHA : string;
+export declare const RFC2396_ALPHA : string;
+export declare const RFC2396_ALPHANUM : string;
+export declare const RFC3986_UNRESERVED : string;
+export declare const RFC3986_SUBDELIMS : string;
+export declare const RFC3986_PCT_ENCODED : string;
+export declare const RFC3986_REG_NAME : string;
+export declare const RFC3986_PCHAR : string;
+export declare const RFC3986_QUERY : string;
+export declare const RFC3986_SEGMENT : string;
+export declare const RFC3986_FRAGMENT : string;
+export declare const RFC3986_PATH_SEGMENTS : string;
+export declare const PCHAR_TOKENIZER : RegExp;
+
+declare interface uriObj {
+    scheme: string;
+    host: string;
+    port: string;
+    userInfo: string;
+    authority: string;
+    path: string;
+    query: string;
+    fragment: string;
+}
 
 /**
- * TODO: get rid of RFC2396 constants
+ * @param {string} uriStr
+ * @return {uriObj}
  */
-export declare var RFC2396_DIGIT : string;
+declare function decomposeComponents(uriStr : string): uriObj;
 
 /**
- *
- */
-export declare var RFC2396_LOWALPHA : string;
-
-/**
- *
- */
-export declare var RFC2396_UPALPHA : string;
-
-/**
- *
- */
-export declare var RFC2396_ALPHA : string;
-
-/**
- *
- */
-export declare var RFC2396_ALPHANUM : string;
-
-/**
- *
- */
-export declare var RFC3986_UNRESERVED : string;
-
-/**
- *
- */
-export declare var RFC3986_SUBDELIMS : string;
-
-/**
- *
- */
-export declare var RFC3986_PCT_ENCODED : string;
-
-/**
- *
- */
-export declare var RFC3986_REG_NAME : string;
-
-/**
- *
- */
-export declare var RFC3986_PCHAR : string;
-
-/**
- *
- */
-export declare var RFC3986_QUERY : string;
-
-/**
- *
- */
-export declare var RFC3986_SEGMENT : string;
-
-/**
- *
- */
-export declare var RFC3986_FRAGMENT : string;
-
-/**
- *
- */
-export declare var RFC3986_PATH_SEGMENTS : string;
-
-/**
- * TODO: get rid of RFC2396 constants
- * @param uriStr
+ * @param {string} userInfo
+ * @param {string} host
+ * @param {string} port
  * @return
  */
-declare function decomposeComponents(uriStr : any): any;
-
-/**
- *
- * @param userInfo
- * @param host
- * @param port
- * @return
- */
-declare function recomposeAuthorityComponents(userInfo : any, host : any, port : any): any;
+declare function recomposeAuthorityComponents(userInfo : string, host : string, port : string): string;
 
 /**
  *
@@ -100,6 +54,7 @@ declare function recomposeAuthorityComponents(userInfo : any, host : any, port :
  * @param host
  * @param port
  */
+
 declare function _checkAuthorityInvariant(authority : any, userInfo : any, host : any, port : any): void;
 
 /**
@@ -107,169 +62,139 @@ declare function _checkAuthorityInvariant(authority : any, userInfo : any, host 
  * Remarks:
  * defined(x) is coded with !=null (means undefined and null are handled the same way)
  * ignores "authority sub components"
- * *
- * @param undefined
- * @param undefined
- * @param undefined
- * @param undefined
- * @param undefined
- * @param undefined
- * @param undefined
- * @param fragment}
- * @return
+ * @param {uriObj} obj
+ * @return {string}
  */
-declare function recomposeComponents(param1 : /* {scheme */ any, param2 : /* authority */ any, param3 : /* userInfo */ any, param4 : /* host */ any, param5 : /* port */ any, param6 : /* path */ any, param7 : /* query */ any, fragment : any):  /* error */ any;
+declare function recomposeComponents(obj: uriObj): string;
+
+/**
+ * @param {string} str
+ * @param {string} legalRange
+ * @return {string}
+ */
+declare function percentEncode(str : string, legalRange : string): string;
 
 /**
  *
- * @param str
- * @param legalRange
- * @return
- */
-declare function percentEncode(str : any, legalRange : string): string;
-
-/**
- *
- * @param path
- * @return
+ * @param {string} path
+ * @return {string}
  */
 declare function removeDotSegments(path : string): string;
 
-/**
- * 5.2.3.  Merge Paths
- * *
- * @param undefined
- * @param path}
- * @param refPath
- * @return
- */
-declare function _merge(param1 : /* {authority */ any, path : any, refPath : string): any;
+declare function _merge({authority, path} : uriObj, refPath : string): string;
 
 /**
  * 5.2.2.  Transform References
- * *
- * @param base
- * @param undefined
- * @param undefined
- * @param undefined
- * @param undefined
- * @param undefined
- * @param undefined
- * @param undefined
- * @param fragment}
- * @return
+ * @param {uriObj} base
+ * @param {uriObj} ref
+ * @return {uriObj}
  */
-declare function _transformReference(base : any, param2 : /* {scheme */ any, param3 : /* authority */ any, param4 : /* userInfo */ any, param5 : /* host */ any, param6 : /* port */ any, param7 : /* path */ any, param8 : /* query */ any, fragment : any): any;
+declare function _transformReference(base : uriObj, { scheme, authority, userInfo, host, port, path, query, fragment }: uriObj) : uriObj;
 
 /**
  * 5.2.1.  Pre-parse the Base URI
  * *
  * @param {scheme}
  */
-declare function _preParseBaseUri({scheme} : any): void;
+declare function _preParseBaseUri({scheme} : uriObj): void;
 
 /**
  *
- * @param base
- * @param ref
- * @return
+ * @param {uriObj} base
+ * @param {uriObj} ref
+ * @return {uriObj}
  */
-declare function resolve(base : any, ref : any): /* _transformReference.!ret */ any;
+declare function resolve(base : uriObj, ref : uriObj): uriObj;
 
 /**
  *
- * @param encodedPath
- * @return
+ * @param {string} encodedPath
+ * @return {Array<string>}
  */
-declare function decodeSegments(encodedPath : any): any;
+declare function decodeSegments(encodedPath : string): Array<string>;
 
 /**
  *
- * @param segments
- * @return
+ * @param {Array<string>} segments
+ * @return {string}
  */
-declare function encodeSegments(segments : any): string;
+declare function encodeSegments(segments : Array<string>): string;
 
 /**
  *
- * @param uriParent
- * @param uriSub
- * @param orSame
- * @return
+ * @param {uriObj} uriParent
+ * @param {uriObj} uriSub
+ * @param {boolean} orSame
+ * @return {boolean}
  */
-declare function isSubordinate(uriParent : any, uriSub : any, orSame : any): any;
+declare function isSubordinate(uriParent : uriObj, uriSub : uriObj, orSame : boolean): boolean;
 
 /**
  *
- * @param segment
- * @return
+ * @param {string} segment
+ * @return {string}
  */
-declare function encodeSegment(segment : any): string;
+declare function encodeSegment(segment : string): string;
 
 /**
  *
- * @param str
- * @return
+ * @param {string} str
+ * @return {string}
  */
-declare function encodeQuery(str : any): string;
+declare function encodeQuery(str : string): string;
 
 /**
  *
- * @param str
- * @return
+ * @param {string} str
+ * @return {string}
  */
-declare function encodeFragment(str : any): string;
+declare function encodeFragment(str : string): string;
 
 /**
  *
- * @param raw
- * @param legalRange
- * @param doThrow
- * @return
+ * @param {string} raw
+ * @param {string} legalRange
+ * @param {string} doThrow
+ * @return {null | Error}
  */
-declare function checkEncoding(raw : any, legalRange : string, doThrow : any): Error;
+declare function checkEncoding(raw : string, legalRange : string, doThrow : string): Error | null;
 
 /**
  *
- * @param str
- * @param doThrow
- * @return
+ * @param {string} str
+ * @param {string} doThrow
+ * @return {null | Error}
  */
-declare function checkSegmentsEncoding(str : any, doThrow : any): Error;
+declare function checkSegmentsEncoding(str : string, doThrow : string): Error | null;
 
 /**
  *
- * @param str
- * @param doThrow
- * @return
+ * @param {string} str
+ * @param {string} doThrow
+ * @return {null | Error}
  */
-declare function checkSegmentEncoding(str : any, doThrow : any): Error;
+declare function checkSegmentEncoding(str : string, doThrow : string): Error | null;
 
 /**
  *
- * @param str
- * @param doThrow
- * @return
+ * @param {string} str
+ * @param {string} doThrow
+ * @return {null | Error}
  */
-declare function checkQueryEncoding(str : any, doThrow : any): Error;
+declare function checkQueryEncoding(str : string, doThrow : string): Error | null;
 
 /**
  *
- * @param str
- * @param doThrow
- * @return
+ * @param {string} str
+ * @param {string} doThrow
+ * @return {null | Error}
  */
-declare function checkFragmentEncoding(str : any, doThrow : any): Error;
+declare function checkFragmentEncoding(str : string, doThrow : string): Error | null;
 
 /**
  *
- * @param query
- * @param bDecode
- * @return
+ * @param {string} query
+ * @param {boolean} bDecode
+ * @return {object}
  */
-declare function parseQuery(query : any, bDecode : any): any;
-
-/**
- *
- */
-export declare var PCHAR_TOKENIZER : RegExp;
+declare function parseQuery(query : string, bDecode : boolean): object;
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,3 @@
 import * as uri from './_uri';
 import * as Uri from './uri';
-export {uri, Uri};
+export { uri, Uri };
