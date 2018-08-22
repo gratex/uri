cbad113b334fb7ac95264df256bfddbf4acea848
Adrian Rakovsky <adrian.rakovsky@gratex.com>
2018-08-22 10:35:46 +0200

    [FIX] deleted files are not linted in hook now

--- scripts/lint-hook.sh
+++ scripts/lint-hook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files="$(git diff --name-only --cached | grep -E '^(src|test).*\.js$')"
+files="$(git diff --name-only --cached --diff-filter=d | grep -E '^(src|test).*\.js$')"
 if [[ $files ]];then
   eslint --max-warnings 0 $files
 fi
